fec47e7e5ea737d96a8d50808ee91d384c8fdb95
iamsam786 <samukelondlela2.0@gmail.com>
2025-07-20 02:11:36 +0200

    Initial commit - SpringBoot Project

---
+++ marketplace/src/main/java/com/samukelo/marketplace/AwThengeApplication.java
@@ -0,0 +1,13 @@
+package com.samukelo.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwThengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwThengeApplication.class, args);
+	}
+
+}
---
+++ marketplace/src/test/java/com/samukelo/marketplace/AwThengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samukelo.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwThengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
